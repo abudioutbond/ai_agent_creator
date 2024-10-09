12c68e6a7ff881cce1843e7f4f3f1ab5bca66c57
Abudi <abdelrahman.mohamed@digitalcareerinstitute.org>
2024-10-09 18:26:26 +0200

    disable comunity share

--- backend/open_webui/config.py
+++ backend/open_webui/config.py
@@ -753,7 +753,7 @@ ENABLE_ADMIN_CHAT_ACCESS = (
 ENABLE_COMMUNITY_SHARING = PersistentConfig(
     "ENABLE_COMMUNITY_SHARING",
     "ui.enable_community_sharing",
-    os.environ.get("ENABLE_COMMUNITY_SHARING", "True").lower() == "true",
+    os.environ.get("ENABLE_COMMUNITY_SHARING", "False").lower() == "frue",
 )
 
 ENABLE_MESSAGE_RATING = PersistentConfig(
