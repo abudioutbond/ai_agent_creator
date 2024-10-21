0080d2b253f76fdcf8cd6aa2ec63bd900e2aea80
Abudi <abdelrahman.mohamed@digitalcareerinstitute.org>
2024-10-21 04:19:33 +0200

    change backend url to outbond

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,9 @@
-import { browser, dev } from '$app/environment';
+import { browser } from '$app/environment';
 // import { version } from '../../package.json';
 
 export const APP_NAME = 'Open WebUI';
 
-export const WEBUI_HOSTNAME = browser ? (dev ? `${location.hostname}:8080` : ``) : '';
-export const WEBUI_BASE_URL = browser ? (dev ? `http://${WEBUI_HOSTNAME}` : ``) : ``;
+export const WEBUI_BASE_URL = browser ? 'https://ai-backend.outbond.io' : '';
 export const WEBUI_API_BASE_URL = `${WEBUI_BASE_URL}/api/v1`;
 
 export const OLLAMA_API_BASE_URL = `${WEBUI_BASE_URL}/ollama`;
